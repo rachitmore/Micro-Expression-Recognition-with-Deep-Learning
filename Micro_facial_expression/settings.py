6cf1e975b78ce165d2027355a99ffa5e54c9b9ae
Rachit More <rachitmore3@gmail.com>
2024-08-09 14:38:29 +0530

    base django project has been configurated

--- Micro_facial_expression/settings.py
+++ Micro_facial_expression/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'home',
+    'micro_expression',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = "Micro_facial_expression.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR,'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+import os
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ micro_expression/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MicroExpressionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "micro_expression"
